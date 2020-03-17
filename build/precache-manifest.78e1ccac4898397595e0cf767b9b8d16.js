be0b9a5722c686800962738f24e3ae0be64d1b33
Eliot Sanford <eliot587@gmail.com>
2020-03-17 14:48:38 -0500

    update: updated react dependencies and npm audit - react files from summer 2019 jsforwp conf workshop

---
+++ build/precache-manifest.78e1ccac4898397595e0cf767b9b8d16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e65c727c5e549b433cd6767bafcf8c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeec3b0bd577faed0b65",
+    "url": "/static/css/main.95c975f8.chunk.css"
+  },
+  {
+    "revision": "691bbf8974faa93d7993",
+    "url": "/static/js/2.1e203983.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1e203983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeec3b0bd577faed0b65",
+    "url": "/static/js/main.0bbd64bc.chunk.js"
+  },
+  {
+    "revision": "f02c2c4374d0981fea45",
+    "url": "/static/js/runtime-main.18399c82.js"
+  }
+]);
